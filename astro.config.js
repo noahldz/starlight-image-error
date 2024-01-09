df4f2a68bd53b4010aff77aff33054d9b535ff41
ariiiera <151569851+noahldz@users.noreply.github.com>
2024-01-09 14:30:35 -0500

    Initial Commit

---
+++ astro.config.js
@@ -0,0 +1,10 @@
+/* eslint-disable import/no-unresolved */
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [starlight({
+    title: 'Test Docs',
+  })],
+});
